fde0ba3c88dabfc05d269ac5e608bccd099b08ff
Hongming <hongmingli999@gmail.com>
2026-03-13 22:35:27 -0500

    add build script

---
+++ scripts/build.js
@@ -0,0 +1,33 @@
+const path = require('node:path');
+const { mkdir, rm, writeFile } = require('node:fs/promises');
+
+const ncc = require('@vercel/ncc');
+
+async function build() {
+    const rootDir = path.resolve(__dirname, '..');
+    const entryFile = path.join(rootDir, 'src', 'index.js');
+    const releaseDir = path.join(rootDir, 'release');
+    const outputFile = path.join(releaseDir, 'index.js');
+
+    await rm(releaseDir, { recursive: true, force: true });
+    await mkdir(releaseDir, { recursive: true });
+
+    const { code, assets } = await ncc(entryFile, {
+        cache: false,
+        sourceMap: false,
+    });
+
+    const emittedAssets = Object.keys(assets);
+
+    if (emittedAssets.length > 0) {
+        throw new Error(`Build emitted unexpected extra assets: ${emittedAssets.join(', ')}`);
+    }
+
+    await writeFile(outputFile, code, 'utf8');
+    process.stdout.write(`Built ${path.relative(rootDir, outputFile)}\n`);
+}
+
+build().catch((error) => {
+    process.stderr.write(`${error.stack || error.message}\n`);
+    process.exitCode = 1;
+});
